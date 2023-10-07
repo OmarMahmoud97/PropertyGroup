55ff23a18bacf31ce4a3987c36cc7422c6919c16
Omar Mahmoud <99248837+OmarMahmoud97@users.noreply.github.com>
2023-10-07 20:10:11 +0100

    add faqs

--- src/app/maps/page.jsx
+++ src/app/maps/page.jsx
@@ -1,6 +1,5 @@
-"use client";
 import React from "react";
-import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
 const containerStyle = {
   width: "400px",
@@ -11,14 +10,13 @@ const center = {
   lat: 36.733422,
   lng: 3.104153,
 };
+
 function MyMap() {
   return (
     <LoadScript googleMapsApiKey={process.env.GOOGLE_MAPS_API_KEY}>
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={10}
-      ></GoogleMap>
+      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
+        <Marker position={center} />
+      </GoogleMap>
     </LoadScript>
   );
 }
