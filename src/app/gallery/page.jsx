7b88a2b376ce449c65571671dd3e8b90bb42da09
Omar Mahmoud <99248837+OmarMahmoud97@users.noreply.github.com>
2023-10-07 15:53:45 +0100

    add pagination functionality to gallery

--- src/app/gallery/page.jsx
+++ src/app/gallery/page.jsx
@@ -5,48 +5,77 @@ import Modal from "../utils/modal";
 
 const Gallery = () => {
   const [selectedImageIndex, setSelectedImageIndex] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1); // New state to manage current page
+
   const totalImages = 130;
+  const imagesPerPage = 25; // Number of images per page
+  const totalPages = Math.ceil(totalImages / imagesPerPage);
+
   const imagePath = "/assets/pictures";
   const imageFormat = ".jpg";
 
-  const handlePrev = () => {
+  const handlePrevImage = () => {
     if (selectedImageIndex > 0) {
       setSelectedImageIndex(selectedImageIndex - 1);
     }
   };
 
-  const handleNext = () => {
+  const handleNextImage = () => {
     if (selectedImageIndex < totalImages - 1) {
       setSelectedImageIndex(selectedImageIndex + 1);
     }
   };
+
   const padWithZeros = (number, length = 3) => {
     return String(number).padStart(length, "0");
   };
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  // Slicing the array of images to display only the ones for the current page
+  const imagesToDisplay = Array.from({ length: totalImages }).slice(
+    (currentPage - 1) * imagesPerPage,
+    currentPage * imagesPerPage
+  );
 
   return (
     <div className="flex items-center justify-center flex-col">
       <h1 className="text-3xl font-bold">Gallery</h1>
       <div className="flex flex-wrap justify-center lg:p-4">
-        {Array.from({ length: totalImages }).map((_, index) => (
-          <div key={index} className="m-2">
-            <Image
-              src={`${imagePath}/image${padWithZeros(index + 1)}${imageFormat}`}
-              alt={`Image ${index + 1}`}
-              className="w-64 h-64 object-cover rounded shadow cursor-pointer"
-              width={200}
-              height={200}
-              unoptimized={true}
-              onClick={() => setSelectedImageIndex(index)}
-            />
-          </div>
-        ))}
+        {imagesToDisplay.map((_, index) => {
+          const actualIndex = (currentPage - 1) * imagesPerPage + index;
+          return (
+            <div key={actualIndex} className="m-2">
+              <Image
+                src={`${imagePath}/image${padWithZeros(
+                  actualIndex + 1
+                )}${imageFormat}`}
+                alt={`Image ${actualIndex + 1}`}
+                className="w-64 h-64 object-cover rounded shadow cursor-pointer"
+                width={200}
+                height={200}
+                unoptimized={true}
+                onClick={() => setSelectedImageIndex(actualIndex)}
+              />
+            </div>
+          );
+        })}
 
         {selectedImageIndex !== null && (
           <Modal
             onClose={() => setSelectedImageIndex(null)}
-            onPrev={handlePrev}
-            onNext={handleNext}
+            onPrev={handlePrevImage}
+            onNext={handleNextImage}
           >
             <Image
               src={`${imagePath}/image${padWithZeros(
@@ -58,6 +87,15 @@ const Gallery = () => {
             />
           </Modal>
         )}
+      </div>
+      <div className="mt-4">
+        <button onClick={handlePrevPage} disabled={currentPage === 1}>
+          Previous
+        </button>
+        <span className="mx-4">{`${currentPage} / ${totalPages}`}</span>
+        <button onClick={handleNextPage} disabled={currentPage === totalPages}>
+          Next
+        </button>
       </div>
     </div>
   );
