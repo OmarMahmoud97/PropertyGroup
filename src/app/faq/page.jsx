bfbc1de3783e432798a2c0a206b99d09d2e49d43
Omar Mahmoud <99248837+OmarMahmoud97@users.noreply.github.com>
2023-10-07 19:32:35 +0100

    add faqs

--- src/app/faq/page.jsx
+++ src/app/faq/page.jsx
@@ -11,7 +11,7 @@ function FAQ() {
         >
           <summary class="flex cursor-pointer items-center justify-between gap-1.5 text-gray-900">
             <h2 class="font-medium">
-              Lorem ipsum dolor sit amet consectetur adipisicing?
+              Which materials are utilized in your products?
             </h2>
 
             <span class="relative h-5 w-5 shrink-0">
@@ -48,16 +48,19 @@ function FAQ() {
           </summary>
 
           <p class="mt-4 leading-relaxed text-gray-700">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic
-            veritatis molestias culpa in, recusandae laboriosam neque aliquid
-            libero nesciunt voluptate dicta quo officiis explicabo consequuntur
-            distinctio corporis earum similique!
+            We employ only the finest German and European woods in our
+            creations, incorporating distinguished brands such as One Skin,
+            Egger, and Wood Board. Our extensive range encompasses a selection
+            of products and materials including HDF, MDF, and Melamine.
+            Furthermore, we source top-tier kitchen and furniture accessories
+            from premier manufacturers like Bloom, Salice, Grass, and Itech.
           </p>
         </details>
         <details class="group rounded-lg bg-gray-50 p-6 [&_summary::-webkit-details-marker]:hidden">
           <summary class="flex cursor-pointer items-center justify-between gap-1.5 text-gray-900">
             <h2 class="font-medium">
-              Lorem ipsum dolor sit amet consectetur adipisicing?
+              What is the anticipated timeline for material preparation and
+              installation?
             </h2>
 
             <span class="relative h-5 w-5 shrink-0">
@@ -94,16 +97,19 @@ function FAQ() {
           </summary>
 
           <p class="mt-4 leading-relaxed text-gray-700">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic
-            veritatis molestias culpa in, recusandae laboriosam neque aliquid
-            libero nesciunt voluptate dicta quo officiis explicabo consequuntur
-            distinctio corporis earum similique!
+            The initial phase involves meticulous design planning using our
+            advanced 3D computer software. Post this phase, the production
+            process typically spans between 2 to 3 weeks, subject to the
+            intricacies of the design and requisite modifications. The
+            installation process is executed over a span of up to 3 days,
+            culminating with a comprehensive review to ensure it aligns
+            perfectly with your specifications.
           </p>
         </details>
         <details class="group rounded-lg bg-gray-50 p-6 [&_summary::-webkit-details-marker]:hidden">
           <summary class="flex cursor-pointer items-center justify-between gap-1.5 text-gray-900">
             <h2 class="font-medium">
-              Lorem ipsum dolor sit amet consectetur adipisicing?
+              Where can we view examples of your past projects?
             </h2>
 
             <span class="relative h-5 w-5 shrink-0">
@@ -140,16 +146,15 @@ function FAQ() {
           </summary>
 
           <p class="mt-4 leading-relaxed text-gray-700">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic
-            veritatis molestias culpa in, recusandae laboriosam neque aliquid
-            libero nesciunt voluptate dicta quo officiis explicabo consequuntur
-            distinctio corporis earum similique!
+            We invite you to explore our curated gallery located at the top
+            right corner of our homepage for a visual representation of our
+            craftsmanship.
           </p>
         </details>
         <details class="group rounded-lg bg-gray-50 p-6 [&_summary::-webkit-details-marker]:hidden">
           <summary class="flex cursor-pointer items-center justify-between gap-1.5 text-gray-900">
             <h2 class="font-medium">
-              Lorem ipsum dolor sit amet consectetur adipisicing?
+              Which color palettes and textures are available?
             </h2>
 
             <span class="relative h-5 w-5 shrink-0">
@@ -186,17 +191,15 @@ function FAQ() {
           </summary>
 
           <p class="mt-4 leading-relaxed text-gray-700">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic
-            veritatis molestias culpa in, recusandae laboriosam neque aliquid
-            libero nesciunt voluptate dicta quo officiis explicabo consequuntur
-            distinctio corporis earum similique!
+            As collaborators with numerous esteemed European suppliers, we boast
+            an eclectic array of colors and textures. We welcome you to visit
+            our office to personally experience the diversity of designs and
+            tactile appeal of our offerings.
           </p>
         </details>
         <details class="group rounded-lg bg-gray-50 p-6 [&_summary::-webkit-details-marker]:hidden">
           <summary class="flex cursor-pointer items-center justify-between gap-1.5 text-gray-900">
-            <h2 class="font-medium">
-              Lorem ipsum dolor sit amet consectetur adipisicing?
-            </h2>
+            <h2 class="font-medium">How is the pricing determined?</h2>
 
             <span class="relative h-5 w-5 shrink-0">
               <svg
@@ -232,148 +235,11 @@ function FAQ() {
           </summary>
 
           <p class="mt-4 leading-relaxed text-gray-700">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic
-            veritatis molestias culpa in, recusandae laboriosam neque aliquid
-            libero nesciunt voluptate dicta quo officiis explicabo consequuntur
-            distinctio corporis earum similique!
-          </p>
-        </details>
-        <details class="group rounded-lg bg-gray-50 p-6 [&_summary::-webkit-details-marker]:hidden">
-          <summary class="flex cursor-pointer items-center justify-between gap-1.5 text-gray-900">
-            <h2 class="font-medium">
-              Lorem ipsum dolor sit amet consectetur adipisicing?
-            </h2>
-
-            <span class="relative h-5 w-5 shrink-0">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="absolute inset-0 opacity-100 group-open:opacity-0"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="#C7B365"
-                strokeWidth="2"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="absolute inset-0 opacity-0 group-open:opacity-100"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="#C7B365"
-                strokeWidth="2"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-            </span>
-          </summary>
-
-          <p class="mt-4 leading-relaxed text-gray-700">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic
-            veritatis molestias culpa in, recusandae laboriosam neque aliquid
-            libero nesciunt voluptate dicta quo officiis explicabo consequuntur
-            distinctio corporis earum similique!
-          </p>
-        </details>
-        <details class="group rounded-lg bg-gray-50 p-6 [&_summary::-webkit-details-marker]:hidden">
-          <summary class="flex cursor-pointer items-center justify-between gap-1.5 text-gray-900">
-            <h2 class="font-medium">
-              Lorem ipsum dolor sit amet consectetur adipisicing?
-            </h2>
-
-            <span class="relative h-5 w-5 shrink-0">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="absolute inset-0 opacity-100 group-open:opacity-0"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="#C7B365"
-                strokeWidth="2"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="absolute inset-0 opacity-0 group-open:opacity-100"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="#C7B365"
-                strokeWidth="2"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-            </span>
-          </summary>
-
-          <p class="mt-4 leading-relaxed text-gray-700">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic
-            veritatis molestias culpa in, recusandae laboriosam neque aliquid
-            libero nesciunt voluptate dicta quo officiis explicabo consequuntur
-            distinctio corporis earum similique!
-          </p>
-        </details>
-        <details class="group rounded-lg bg-gray-50 p-6 [&_summary::-webkit-details-marker]:hidden">
-          <summary class="flex cursor-pointer items-center justify-between gap-1.5 text-gray-900">
-            <h2 class="font-medium">
-              Lorem ipsum dolor sit amet consectetur adipisicing?
-            </h2>
-
-            <span class="relative h-5 w-5 shrink-0">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="absolute inset-0 opacity-100 group-open:opacity-0"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="#C7B365"
-                strokeWidth="2"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="absolute inset-0 opacity-0 group-open:opacity-100"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="#C7B365"
-                strokeWidth="2"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-            </span>
-          </summary>
-
-          <p class="mt-4 leading-relaxed text-gray-700">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic
-            veritatis molestias culpa in, recusandae laboriosam neque aliquid
-            libero nesciunt voluptate dicta quo officiis explicabo consequuntur
-            distinctio corporis earum similique!
+            The cost is contingent upon several factors including the
+            dimensions, design intricacy, and material selections. However, we
+            pride ourselves in providing unparalleled quality at the most
+            competitive prices, encompassing both product and installation
+            services.
           </p>
         </details>
       </div>
---
+++ src/app/maps/page.jsx
@@ -0,0 +1,27 @@
+"use client";
+import React from "react";
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
+
+const containerStyle = {
+  width: "400px",
+  height: "400px",
+};
+
+const center = {
+  lat: 42,
+  lng: 91,
+};
+
+function MyMap() {
+  return (
+    <LoadScript googleMapsApiKey="YOUR_API_KEY">
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+      ></GoogleMap>
+    </LoadScript>
+  );
+}
+
+export default MyMap;
