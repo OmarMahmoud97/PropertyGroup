aa374bc924b1120267f5ba41a3087424995eb321
Omar Mahmoud <99248837+OmarMahmoud97@users.noreply.github.com>
2023-10-11 00:31:10 +0100

    add honeypot to contact form

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -27,6 +27,7 @@ function Contact() {
                 <label className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none">
                   Name
                 </label>
+                <input type="text" name="_honey" style="display:none" />
               </div>
               <div className="relative mb-6" data-te-input-wrapper-init>
                 <input
